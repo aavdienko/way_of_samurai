58820da04a701cbb8369590cf5465416d040a16f
aavdienko <asiavostok@seznam.cz>
2022-11-26 15:32:16 +0100

    Lesson 94. Added React.lazy to News, Music, DialogsContainer.V1.0.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Settings from './Components/Settings/Settings';
-import News from './Components/News/News';
-import Music from './Components/Music/Music';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DialogsContainer from './Components/Dialogs/DialogsContainer';
 import UsersContainer from './Components/Users/UsersContainer';
 import ProfileContainer from './Components/Profile/ProfileContainer';
 import HeaderContainer from './Components/Header/HeaderContainer';
 import Login from './Components/Login/Login';
-import { initializeApp } from './Redux/app-reducer'
+import { initializeApp } from './Redux/app-reducer';
 import Preloader from './Components/Common/Preloader/Preloader';
-import {connect} from "react-redux";
+import { connect } from 'react-redux';
+
+const DialogsContainer = React.lazy( () => import('./Components/Dialogs/DialogsContainer'))
+const News = React.lazy( () => import('./Components/News/News'))
+const Music = React.lazy( () => import('./Components/Music/Music'))
+
+// Создали React.lazy для этих компонент:
+// import DialogsContainer from './Components/Dialogs/DialogsContainer';
+// import News from './Components/News/News';
+// import Music from './Components/Music/Music';
 
 class App extends React.Component {
   componentDidMount() {
@@ -24,35 +30,51 @@ class App extends React.Component {
       return <Preloader />;
     }
     return (
-    <BrowserRouter>
-      <div className="app-wraper">
-        <HeaderContainer />
-        <Navbar />
-        <div className="app-wraper-content">
-          <Routes>
-            <Route path="/profile/:userId" element={<ProfileContainer />} />
-            <Route path="/profile/" element={<ProfileContainer />} />
-            <Route path="/dialogs/*" element={<DialogsContainer />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/music" element={<Music />} />
-            <Route path="/users" element={<UsersContainer />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
+      <BrowserRouter>
+        <div className="app-wraper">
+          <HeaderContainer />
+          <Navbar />
+          <div className="app-wraper-content">
+            <Routes>
+              <Route path="/profile/:userId" element={<ProfileContainer />} />
+              <Route path="/profile/" element={<ProfileContainer />} />
+              <Route
+                path="/dialogs/*"
+                element={
+                  <Suspense fallback={<div><Preloader /></div>}>
+                    <DialogsContainer />
+                  </Suspense>
+                }
+              />
+              <Route 
+                path="/news" 
+                element={
+                  <Suspense fallback={<div><Preloader /></div>}>
+                    <News />
+                  </Suspense>
+              } />
+              <Route 
+                path="/music" 
+                element={
+                  <Suspense fallback={<div><Preloader /></div>}>
+                    <Music />
+                  </Suspense>
+                } />
+              <Route path="/users" element={<UsersContainer />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
-    )
+      </BrowserRouter>
+    );
   }
 }
 
 let mapStateToProps = (state) => ({
+  initialized: state.app.initialized,
+});
 
-  initialized: state.app.initialized
-
-})
-
-
-export default connect( mapStateToProps, {initializeApp} )( App );
+export default connect(mapStateToProps, { initializeApp })(App);
 
 // export default compose(withRouter, connect(null, { getAuthUserData }))(App);
